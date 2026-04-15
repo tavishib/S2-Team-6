1b97b7dfe86cdea32742b24ab7c9f882814329aa
jav4Aung <aungag1998@gmail.com>
2026-04-15 09:00:31 -0700

    Updated: DBConnection.java, fixed driver not found error

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -6,9 +6,14 @@ public class DBConnection {
 
     private static final String URL      = "jdbc:mysql://localhost:3306/StudyMatch";
     private static final String USER     = "root";
-    private static final String PASSWORD = "CS157A@sjsu";
+    private static final String PASSWORD = "mysql@1234";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
